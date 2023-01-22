0ca6790fd051d9e41803a7c5d90d28d2a173c3d5
Mike Pattyn <mike.pattyn1@gmail.com>
2023-01-22 16:09:19 +0100

    small key

--- Flyingdarts-Signalling-Shared/Fields.cs
+++ Flyingdarts-Signalling-Shared/Fields.cs
@@ -2,7 +2,7 @@
 public static class Fields
 {
     public static string ConnectionId = "ConnectionId";
-    public static string RoomId = "RoomId";
+    public static string RoomId = "roomId";
     public static string PlayerId = "PlayerId";
     public static string PlayerName = "PlayerName";
     public static string CurrentScore = "CurrentScore";
