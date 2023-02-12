d212a4cf0def63a7ee0934c910e47ac1752e6d59
Mike Pattyn <mike.pattyn1@gmail.com>
2023-02-12 08:44:21 +0100

    same function def

--- Flyingdarts-Rooms-OnJoin/Function.cs
+++ Flyingdarts-Rooms-OnJoin/Function.cs
@@ -8,19 +8,15 @@ using Amazon.Lambda.Serialization.SystemTextJson;
 using Flyingdarts.Requests.Rooms.Join;
 using Flyingdarts.Rooms.OnJoin;
 
-var _serializer = new DefaultLambdaJsonSerializer(x => x.PropertyNameCaseInsensitive = true);
-var _dynamoDbClient = new AmazonDynamoDBClient();
-var _tableName = Environment.GetEnvironmentVariable("TableName")!;
-var _webSocketApiUrl = Environment.GetEnvironmentVariable("WebSocketApiUrl")!;
-AmazonApiGatewayManagementApiClient _apiGatewayClient = new(new AmazonApiGatewayManagementApiConfig { ServiceURL = _webSocketApiUrl });
-var innerHandler = new OnJoinHandler(_dynamoDbClient, _tableName);
+var serializer = new DefaultLambdaJsonSerializer(x => x.PropertyNameCaseInsensitive = true);
+var dynamoDbClient = new AmazonDynamoDBClient();
+var tableName = Environment.GetEnvironmentVariable("TableName")!;
+var innerHandler = new OnJoinHandler(dynamoDbClient, tableName);
 // ReSharper disable once ConvertToLocalFunction
 var handler = async (APIGatewayProxyRequest request, ILambdaContext context) =>
 {
-    var socketRequest = request.To<JoinRoomRequest>(_serializer);
-
+    var socketRequest = request.To<JoinRoomRequest>(serializer);
     // await MessageDispatcher.DispatchMessage(context, _dynamoDbClient, _apiGatewayClient, _tableName, data, socketRequest.Message.RoomId);
-
     return await innerHandler.Handle(socketRequest);
 };
 
