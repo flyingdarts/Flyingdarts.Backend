c542878a85f8a64d0728a0906c78188d97dc5c20
Mike Pattyn <mike.pattyn1@gmail.com>
2023-02-12 08:41:37 +0100

    Option response status code

--- Flyingdarts-Signalling-Shared/Responses.cs
+++ Flyingdarts-Signalling-Shared/Responses.cs
@@ -3,12 +3,12 @@ using Amazon.Lambda.APIGatewayEvents;
 namespace Flyingdarts.Signalling.Shared;
 public static class Responses
 {
-    private static APIGatewayProxyResponse Generate(HttpStatusCode code, string body) => new()
+    private static APIGatewayProxyResponse Generate(string body, HttpStatusCode code = HttpStatusCode.Created) => new()
     {
         StatusCode = (int)code,
         Body = body
     };
 
-    public static APIGatewayProxyResponse Created(string body) => Generate(HttpStatusCode.Created, body);
-    public static APIGatewayProxyResponse InternalServerError(string body) => Generate(HttpStatusCode.InternalServerError, body);
+    public static APIGatewayProxyResponse Created(string body) => Generate(body);
+    public static APIGatewayProxyResponse InternalServerError(string body) => Generate(body, HttpStatusCode.InternalServerError);
 }
