66a22cfbc84c40984d5a6123c667976f368b9166
Mike Pattyn <mike.pattyn1@gmail.com>
2023-02-12 02:36:48 +0100

    classify and ignore there

--- Flyingdarts-Signalling-Shared/IHaveAConnectionId.cs
+++ Flyingdarts-Signalling-Shared/IHaveAConnectionId.cs
@@ -2,13 +2,13 @@
 namespace Flyingdarts.Signalling.Shared;
 public interface IHaveAConnectionId
 {
-    [JsonIgnore]
-
     public string ConnectionId { get; set; }
 }
 
-public interface IAmAMessage<TMessage> : IHaveAConnectionId
+public abstract class IAmAMessage<TMessage> : IHaveAConnectionId
 {
     public string Action { get; set; }
     public TMessage Message { get; set; }
+    [JsonIgnore]
+    public string ConnectionId { get; set; }
 }
