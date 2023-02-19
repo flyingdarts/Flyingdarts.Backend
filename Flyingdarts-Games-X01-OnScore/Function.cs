ad8bb6a859e709040a5e480b26725d96b697404f
Mike Pattyn <mike@givtapp.net>
2023-02-19 22:29:42 +0100

    gogogo

--- Flyingdarts-Games-X01-OnScore/Function.cs
+++ Flyingdarts-Games-X01-OnScore/Function.cs
@@ -6,16 +6,21 @@ using Amazon.Lambda.RuntimeSupport;
 using Amazon.Lambda.Serialization.SystemTextJson;
 using Flyingdarts.Games.X01.OnScore;
 using Flyingdarts.Requests.Games.X01.OnScore;
+using Amazon.ApiGatewayManagementApi;
+using System.Text.Json;
 
 var serializer = new DefaultLambdaJsonSerializer(x => x.PropertyNameCaseInsensitive = true);
 var dynamoDbClient = new AmazonDynamoDBClient();
 var tableName = Environment.GetEnvironmentVariable("TableName")!;
 var innerHandler = new OnScoreHandler(dynamoDbClient, tableName);
+var webSocketUrl = Environment.GetEnvironmentVariable("WebSocketApiUrl")!;
+var apiGatewayClient = new AmazonApiGatewayManagementApiClient(new AmazonApiGatewayManagementApiConfig { ServiceURL = webSocketUrl });
+
 // ReSharper disable once ConvertToLocalFunction
 var handler = async (APIGatewayProxyRequest request, ILambdaContext context) =>
 {
     var socketRequest = request.To<X01OnScoreRequest>(serializer);
-    return await innerHandler.Handle(socketRequest, dynamoDbClient, tableName);
+    return await innerHandler.Handle(socketRequest, dynamoDbClient, tableName, context, apiGatewayClient, JsonSerializer.Serialize(socketRequest), socketRequest.Message.RoomId);
 };
 // Build the Lambda runtime client passing in the handler to call for each
 // event and the JSON serializer to use for translating Lambda JSON documents
