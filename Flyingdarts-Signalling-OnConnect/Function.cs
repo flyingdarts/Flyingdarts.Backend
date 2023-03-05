9e65f7f55a80c8bcabd61a8dbc47364789e90b1b
Mike Pattyn <mike.pattyn1@gmail.com>
2023-03-05 08:01:02 +0100

    Remove log

--- Flyingdarts-Signalling-OnConnect/Function.cs
+++ Flyingdarts-Signalling-OnConnect/Function.cs
@@ -14,7 +14,6 @@ var innerHandler = new ConnectHandler(dynamoDbClient, tableName);
 var handler = async (APIGatewayProxyRequest request, ILambdaContext context) =>
 {
     var socketRequest = request.To<PlayerConnectedRequest>(serializer);
-    context.Logger.LogInformation(socketRequest.ToString());
     return await innerHandler.Handle(socketRequest);
 };
 
