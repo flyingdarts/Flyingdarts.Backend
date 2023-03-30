f785eb753fe23b5592ab35049fefdc3902592c7c
Mike Pattyn <mike.pattyn1@gmail.com>
2023-03-30 16:20:44 +0200

    Add slash commands and handler

--- Lambdas/Flyingdarts-Integrations-Discord/IntegrationHandler.cs
+++ Lambdas/Flyingdarts-Integrations-Discord/IntegrationHandler.cs
@@ -4,12 +4,15 @@ using System;
 using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
+using Amazon.Auth.AccessControlPolicy.ActionIdentifiers;
 using Discord;
 using Discord.Commands;
+using Discord.Net;
 using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using Discord.Rest;
 using Flyingdarts.Requests;
+using Newtonsoft.Json;
 
 public class IntegrationHandler
 {
@@ -52,18 +55,14 @@ public class IntegrationHandler
         {
             // Again, log level:
             LogLevel = LogSeverity.Info,
-
-            // There's a few more properties you can set,
-            // for example, case-insensitive commands.
             CaseSensitiveCommands = false,
         });
 
-        // Subscribe the logging handler to both the client and the CommandService.
+
         var log = LoggingService.Instance(_client, _commands);
         _client.Log += log.LogAsync;
         _commands.Log += log.LogAsync;
 
-        // Setup your DI container.
         _services = ConfigureServices(_client, _commands);
     }
     public async Task<APIGatewayProxyResponse> Handle(byte[] discordBody)
@@ -93,69 +92,70 @@ public class IntegrationHandler
             return Responses.InternalServerError($"Failed to send message: {e.Message}");
         }
     }
-
-    // If any services require the client, or the CommandService, or something else you keep on hand,
-    // pass them as parameters into this method as needed.
-    // If this method is getting pretty long, you can seperate it out into another file using partials.
     private static IServiceProvider ConfigureServices(DiscordSocketClient client, CommandService commandService)
     {
         var services = new ServiceCollection()
-            // Repeat this for all the service classes
-            // and other dependencies that your commands might need.
             .AddSingleton(new LoggingService(client, commandService));
-
-        // When all your required services are in the collection, build the container.
-        // Tip: There's an overload taking in a 'validateScopes' bool to make sure
-        // you haven't made any mistakes in your dependency graph.
         return services.BuildServiceProvider(true);
     }
 
     private async Task MainAsync(string token)
     {
-        // Centralize the logic for commands into a separate method.
         await InitCommands();
 
-        // Login and connect.
         await _client.LoginAsync(TokenType.Bot, token);
         await _client.StartAsync();
 
-        // Wait infinitely so your bot actually stays connected.
         await Task.Delay(Timeout.Infinite);
     }
 
+    public async Task SlashCommandHandler(SocketSlashCommand command)
+    {
+        await command.RespondAsync($"You executed {command.Data.Name}");
+    }
+    public async Task Client_Ready()
+    {
+        var guild = _client.GetGuild(1013538880563183616);
+        var guildCommand = new SlashCommandBuilder();
+        guildCommand.WithName("huur");
+        guildCommand.WithDescription("duur");
+
+        var globalCommand = new SlashCommandBuilder();
+        globalCommand.WithName("peb");
+        globalCommand.WithName("cac");
+
+        try
+        {
+            await guild.CreateApplicationCommandAsync(guildCommand.Build());
+
+            await _client.CreateGlobalApplicationCommandAsync(globalCommand.Build());
+        }
+        catch (HttpException e)
+        {
+            await guild.DefaultChannel.SendMessageAsync(JsonConvert.SerializeObject(e.Errors, Formatting.Indented));
+            throw;
+        }
+    }
     private async Task InitCommands()
     {
-        // Either search the program and add all Module classes that can be found.
-        // Module classes MUST be marked 'public' or they will be ignored.
-        // You also need to pass your 'IServiceProvider' instance now,
-        // so make sure that's done before you get here.
         await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
-        // Or add Modules manually if you prefer to be a little more explicit:
-        //await _commands.AddModuleAsync<SomeModule>(_services);
-        // Note that the first one is 'Modules' (plural) and the second is 'Module' (singular).
 
-        // Subscribe a handler to see if a message invokes a command.
+        _client.Ready += Client_Ready;
         _client.MessageReceived += HandleCommandAsync;
+        _client.SlashCommandExecuted += SlashCommandHandler;
     }
 
     private async Task HandleCommandAsync(SocketMessage arg)
     {
-        // Bail out if it's a System Message.
         var msg = arg as SocketUserMessage;
         if (msg == null) return;
 
-        // We don't want the bot to respond to itself or other bots.
         if (msg.Author.Id == _client.CurrentUser.Id || msg.Author.IsBot) return;
 
-        // Create a number to track where the prefix ends and the command begins
         int pos = 0;
-        // Replace the '!' with whatever character
-        // you want to prefix your commands with.
-        // Uncomment the second half if you also want
-        // commands to be invoked by mentioning the bot instead.
-        if (msg.HasCharPrefix('!', ref pos) /* || msg.HasMentionPrefix(_client.CurrentUser, ref pos) */)
+     
+        if (msg.HasCharPrefix('!', ref pos))
         {
-            // Create a Command Context.
             var context = new SocketCommandContext(_client, msg);
             await context.Channel.SendMessageAsync("Ah yeet!");
         }
